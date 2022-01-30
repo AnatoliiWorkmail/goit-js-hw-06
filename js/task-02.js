12c6d2a2b1ca170f51ed197c52e570bb148fcbc2
AnatoliiWorkmail <jp.komod.su@gmail.com>
2022-01-31 00:26:47 +0200

    add-task2
    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+console.log(ingredients);
+
+// const listElement = document.createElement('li');
+// listElement.classList.add('item');
+// listElement.textContent ='Potatoes';
+// console.log(listElement)
+const selectUL = document.querySelector('#ingredients');
+// console.log(selectUL);
+// selectUL.append(listElement);
+
+
+
+function makeLi(name) {
+  const listElement = document.createElement('li');
+  listElement.classList.add('item');
+  listElement.textContent = name;
+  selectUL.append(listElement);
+  return selectUL
+  
+}
+const addList = ingredients.map(makeLi);
+console.log(addList)
