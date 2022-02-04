617c409819128b0d82ba9ff5b374e32b86471cd4
AnatoliiWorkmail <jp.komod.su@gmail.com>
2022-02-04 23:34:22 +0200

    add-task5

---
+++ js/task-05.js
@@ -0,0 +1,22 @@
+
+const selectInput = document.querySelector('#name-input');
+const selectOutput = document.querySelector('#name-output');
+function Name() {
+    let content = selectInput.value;
+   return selectOutput.textContent = content;
+    
+    
+    
+}
+selectInput.addEventListener('input', Name);
+
+
+
+
+
+
+
+
+
+console.log(selectInput);
+console.log(selectOutput);
