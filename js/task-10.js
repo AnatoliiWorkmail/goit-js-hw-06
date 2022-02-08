a093509f69a71656da3eb71445cfc1cc2e8fe57b
AnatoliiWorkmail <jp.komod.su@gmail.com>
2022-02-08 23:34:37 +0200

    add10tak

--- js/task-10.js
+++ js/task-10.js
@@ -5,7 +5,10 @@ const createBtn = document.querySelector('[data-create]');
 const clearBtn = document.querySelector('[data-destroy]');
 const inputEl = document.querySelector('input');
 const boxEl = document.querySelector('#boxes')
-const amount = inputEl.value;
+// inputEl.value = 4;
+// const amount = inputEl.valueAsNumber;
+let amount
+inputEl.addEventListener('change',()=>{return amount=inputEl.valueAsNumber })
 
 function creatBoxDiv(event) {
   event.preventDefault()
@@ -15,10 +18,21 @@ function creatBoxDiv(event) {
   newDivEl.style.width = '30px';
   newDivEl.style.background = getRandomHexColor();
   boxEl.append(newDivEl)
+  for (let i = 10; i <Number(10*amount); i += 10){
+   const newDivEl = document.createElement('div');
+  newDivEl.classList = 'create-box';
+  newDivEl.style.height = `${40+i}px`;
+  newDivEl.style.width = `${40+i}px`;
+  newDivEl.style.background = getRandomHexColor();
+  boxEl.append(newDivEl)
+ }
+}
+function destroyBoxes() {
+  boxEl.innerHTML = '';
 }
 
 createBtn.addEventListener('click',creatBoxDiv)
-
+clearBtn.addEventListener('click',destroyBoxes)
 // createBoxes(amount)
 // destroyBoxes()
 
@@ -28,4 +42,4 @@ createBtn.addEventListener('click',creatBoxDiv)
 
 console.log(createBtn);
 console.log(clearBtn);
-console.log(inputEl.value)
+console.log(inputEl)
